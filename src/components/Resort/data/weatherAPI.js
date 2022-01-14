be64548dbb72833483b85cfc73ab58335c20f580
DannyLisamay <38701066+DannyLisamay@users.noreply.github.com>
2022-01-14 14:53:44 -0500

    Update weatherAPI.js
    Moved API key into .env

--- src/components/Resort/data/weatherAPI.js
+++ src/components/Resort/data/weatherAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const key = "25d0569851c0869a01a553ee05c7ffff";
+const key = process.env.OPENWEATHERMAP_API_KEY;
 export const getWeatherData = async (city) => {
     console.log(city);
     const baseURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key + "&units=imperial";
